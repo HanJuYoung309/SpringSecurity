d37c4fd4fab2091a4a5e65a03402bdd63aa6c3e5
HanJuYoung309 <juyg407@naver.com>
2023-10-18 00:19:48 +0900

    첫번째 커밋

---
+++ src/main/java/me/benny/practice/spring/securtiy/SpringSecurityPracticeApplication.java
@@ -0,0 +1,15 @@
+package me.benny.practice.spring.securtiy;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * SpringSecurity 연습 Application
+ */
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+}
